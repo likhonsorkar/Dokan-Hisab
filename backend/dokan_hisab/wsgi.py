9eef80dfa7e9f1812464d52388e57561d56376cc
unknown <likhonsorkar002@gmail.com>
2026-04-08 10:35:16 +0600

    Django

---
+++ backend/dokan_hisab/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dokan_hisab project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dokan_hisab.settings')
+
+application = get_wsgi_application()
